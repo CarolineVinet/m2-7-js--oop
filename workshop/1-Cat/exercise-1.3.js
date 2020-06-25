5ff9f066ae4dd2c21848aba9053856d1cf7a5fa0
Caroline Vinet <carovinet@gmail.com>
2020-06-25 13:04:36 -0400

    exercise1.3

--- workshop/1-Cat/exercise-1.3.js
+++ workshop/1-Cat/exercise-1.3.js
@@ -22,3 +22,38 @@
 // You decide how much sleep, eat, and play affects your cat's happiness.
 
 // B) call the different methods with appropriate values and then console.log boots to see what happened to the property values.
+
+class Cat {
+  constructor(name, breed) {
+    this.species = "cat";
+    this.name = name;
+    this.breed = breed;
+    this.tiredness = 0;
+    this.hunger = 0;
+    this.loneliness = 0;
+    this.happiness = 0;
+  }
+
+  sleep = (num) => {
+    this.tiredness -= num * 5;
+    this.happiness += num * 2;
+  };
+
+  eat = (kibbles) => {
+    this.hunger -= kibbles / 5;
+    this.happiness += kibbles * 3;
+  };
+
+  play = (minutes) => {
+    this.loneliness -= minutes * 3;
+    this.happiness += minutes * 6;
+  };
+}
+
+const boots = new Cat("boots", "siamese");
+
+boots.sleep(5);
+boots.eat(5);
+boots.play(5);
+
+console.log(boots);
